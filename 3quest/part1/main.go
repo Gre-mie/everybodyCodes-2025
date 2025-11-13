5f896b5cd18d9083334886f61ac6346c2699f6df
Gre-mie <meow31-cat@outlook.com>
2025-11-13 15:03:17 +0000

    Solution for quest 3 part 1

---
+++ 3quest/part1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("../notes/part1.txt")
+	if err != nil {fmt.Printf("ERROR: couldn't read file\n\toriginal err: %v\n", err)}
+	split_data := strings.Split(string(data), ",")
+	
+	content := make([]int, 0)
+	for _, strnum := range split_data {
+		n, err := strconv.Atoi(strnum)
+		if err != nil {fmt.Printf("ERROR: couldn't convert string to number\n\toriginal err: %v\n", err)} else {
+			content = append(content, n)
+		}
+	}
+	slices.Sort(content)
+	containers := slices.Compact(content)
+	total := 0
+	for _, size := range containers {
+		total += size
+	}
+
+	fmt.Println("Result:", total)
+
+}
